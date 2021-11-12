4f54d4d62e2627f635dfe57f742b7d302eaa1afd
HarryChen0506 <chenwenhua0506@sina.com>
2021-11-12 15:22:16 +0800

    feat: h5 dev proxy config

--- config/dev.js
+++ config/dev.js
@@ -5,5 +5,19 @@ module.exports = {
   defineConstants: {
   },
   mini: {},
-  h5: {}
+  h5: {
+    devServer: {
+      port: 10085,
+      host: '0.0.0.0',
+      proxy: {
+        '/api/': {
+          target: "https://api.starjob.cn/",
+          pathRewrite: {
+            '^/api/': '/'
+          },
+          changeOrigin: true
+        }
+      },
+    }
+  }
 }
